644728e6219a3322fc9f1d257b07f481d6e334fc
ogoogo <ogosae.1018173@docomo.ne.jp>
2021-09-02 10:02:36 +0900

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by 生越冴恵 on 2021/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
